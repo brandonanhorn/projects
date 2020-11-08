692a7f06245fd5647959dbf05a4132f9281758f7
brandonanhorn <38738274+brandonanhorn@users.noreply.github.com>
2020-11-08 16:32:53 -0700

    setting up for K-Means

---
+++ clustering_credit_card/k-means.py
@@ -0,0 +1,13 @@
+%matplotlib inline
+
+from sklearn.cluster import KMeans, k_means
+from sklearn.metrics import silhouette_score
+
+import numpy as np
+import pandas as pd
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.style.use('ggplot')
