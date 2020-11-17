5b8bd2a22556ae6fe0a10b0653ac66cb85f7dba5
brandonanhorn <38738274+brandonanhorn@users.noreply.github.com>
2020-11-17 14:23:18 -0700

    kmeans with matplotlib

--- clustering_credit_card/k-means.py
+++ clustering_credit_card/k-means.py
@@ -1,13 +1,36 @@
 %matplotlib inline
+import pandas as pd
 
 from sklearn.cluster import KMeans, k_means
 from sklearn.metrics import silhouette_score
-
-import numpy as np
-import pandas as pd
 
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib
 
-matplotlib.style.use('ggplot')
+df = pd.read_csv("data/CC General.csv")
+
+df.head(1)
+df.columns = map(str.lower, df.columns)
+
+df.info()
+df_x = df.drop("cust_id", axis=1)
+df_x = df_x.fillna(0)
+
+model = KMeans(n_clusters=5)
+model.fit(df_x)
+df["predict"] = model.labels_
+
+df["predict"].value_counts()
+
+plt.figure(figsize=(7,7));
+
+colors = ["red", "green", "blue", "yellow", "black"]
+df ["color"] = df["predict"].map(lambda p: colors[p])
+
+ax = df.plot(
+    kind = "scatter",
+    x = "balance", y= "purchases",
+    figsize = (10,8),
+    c = df["color"]
+)
