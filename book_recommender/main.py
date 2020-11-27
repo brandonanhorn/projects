360bfc1ac835d29e40c8eee161218e7f4e44802c
brandonanhorn <38738274+brandonanhorn@users.noreply.github.com>
2020-11-27 09:09:23 -0700

    updated - working recommender

--- book_recommender/main.py
+++ book_recommender/main.py
@@ -4,33 +4,40 @@ from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
 
 df = pd.read_csv("data/books.csv")
+df = df.fillna(0)
 
-df.head(1
-
-avg_rating_df = df[["title", "average_rating"]]
-avg_rating_df.head(1)
+df.head(1)
 
 def create_csr_X_matrix(df):
-    m_len = len(avg_rating_df.title.unique())
-    book_encoder = LabelEncoder()
-    m = book_encoder.fit_transform(avg_rating_df.title)
-    X = csr_matrix((avg_rating_df["average_rating"]))
-    return X, book_encoder
+    book_title_len = len(df.title.unique())
+    unique_book_len = len(df.isbn13.unique())
 
-X, book_encoder = create_csr_X_matrix(avg_rating_df)
+    title_encoder = LabelEncoder()
+    unique_id_encoder = LabelEncoder()
 
-book = "Gilead"
+    t = title_encoder.fit_transform(df.title)
+    b = unique_id_encoder.fit_transform(df.isbn13)
 
-#def find_me_a_book(book_title, X, k=10):
-book_ind = title_encoder.transform([book])
-#error here
-book_vec = X[book_ind]
-kNN = NearestNeighbors(n_neighbors=k, algorithm="brute", metric='minkowski')
-kNN.fit(X)
-neighbour = kNN.kneighbors(book_vec, return_distance=False)
-close_movies = neighbour.flatten().tolist()
-z = [book_encoder.inverse_transform([m])[0] for m in close_books]
-z = [zi for zi in z if zi != book_title]
+    book_title_len = len(df.title.unique())
+    unique_book_len = len(df.isbn13.unique())
+
+    X = csr_matrix((df["average_rating"], (t, b)), shape=(book_title_len, unique_book_len))
+    return X, title_encoder, unique_id_encoder
+
+
+X, title_encoder, unique_id_encoder = create_csr_X_matrix(df)
+
+book = 'Gilead'
+
+def find_me_a_book_to_read(book_title, X, k=10):
+    book_ind = movie_encoder.transform([book])
+    book_vec = X[book_ind]
+    kNN = NearestNeighbors(n_neighbors=k, algorithm="brute", metric='minkowski')
+    kNN.fit(X)
+    neighbour = kNN.kneighbors(book_vec, return_distance=False)
+    close_books = neighbour.flatten().tolist()
+    z = [title_encoder.inverse_transform([t])[0] for t in close_books]
+    z = [zi for zi in z if zi != book_title]
     return z
 
-find_me_a_book(book, X, k=10)
+find_me_a_book_to_read("Hour Game", X, k=10)
