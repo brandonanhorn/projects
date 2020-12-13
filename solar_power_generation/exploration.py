0f8f9ae0cfbdddf95275588cb8952596b4722e06
brandonanhorn <38738274+brandonanhorn@users.noreply.github.com>
2020-12-12 21:17:46 -0700

    reformatted two separate dfs

--- solar_power_generation/exploration.py
+++ solar_power_generation/exploration.py
@@ -9,24 +9,30 @@ df.info()
 df_1.info()
 
 df.columns = map(str.lower, df.columns)
-df.describe()
-len(df[df["daily_yield"] == 0])
-
-df["plant_id"].value_counts()
-df["date_time"].value_counts()
-df["date_time"] = df["date_time"] + ":00"
-
-
 df_1.columns = map(str.lower, df_1.columns)
+df.describe().T
+df_1.describe().T
 
+len(df[df["daily_yield"] == 0])
+df["plant_id"].value_counts()
+
+df["date_time"].value_counts()
 df_1["date_time"].value_counts()
 
-df_dates = pd.DataFrame(df_1.date_time.str.split('-').tolist(),index=df_1.ambient_temperature)
+df["date_time"] = df["date_time"] + ":00"
 
-df_dates.head(1)
-df_2 = df_dates.reset_index([0,'ambient_temperature'])
+df_column_changes = df.date_time.str.split("-",expand=True)
+df_column_changes.head(1)
+df_column_changes["more_changes"] = df_column_changes[2]
+df_column_changes_1 = df_column_changes.more_changes.str.split(" ", expand=True)
+df_column_changes_1.head(1)
 
-df_2.head(1)
+df_column_changes_1["time"] = df_column_changes_1[0] + "-" + df_column_changes[1] + "-" + df_column_changes[0] + " " + df_column_changes_1[1]
 
-### Possible issue with pandas - not returning what it should.
-df_2[2].str.split(" ")[0]
+df_column_changes_1.head(1)
+
+df["date_time"] = df_column_changes_1["time"]
+
+df.info()
+df.head(1)
+df_1.head(1)
