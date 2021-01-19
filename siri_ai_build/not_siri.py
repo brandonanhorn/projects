0633a3a77e8392d566422f0d5bcc801a932cd701
brandonanhorn <38738274+brandonanhorn@users.noreply.github.com>
2021-01-19 08:38:35 -0700

    updated - speech recognition portion added

--- siri_ai_build/not_siri.py
+++ siri_ai_build/not_siri.py
@@ -1,3 +1,14 @@
 ## build a siri bot that responds to questions
-
 # takes in live chat and responds with information
+import speech_recognition as sr
+
+listener = sr.Recognizer()
+try:
+    with sr.Microphone() as source:
+        print("go ahead, im listening")
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        print(command)
+
+except:
+    pass
